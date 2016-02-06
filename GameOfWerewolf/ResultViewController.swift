7b85b6be29e97ecb073d05e1606e2127ffef071c
kento <kentostephenohara@gmail.com>
2016-02-06 18:07:34 +0900

    have been able to display the result

--- GameOfWerewolf/ResultViewController.swift
+++ GameOfWerewolf/ResultViewController.swift
@@ -14,7 +14,6 @@ class ResultViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     var rolenumberArray: [Int] = []//役職ごとの人数を入れる
     var roleofplayerArray: [String] = []//インデックスのプレイヤーがその役職
-    var playerdisplayArray: [String] = []//役職配列のインデックス順にプレイヤーを並び替え→必要なし？(2月6日)
     
     var worksections: [String] = ["人狼", "市民", "てるてる"]
     var werewolfitems: [String] = ["人狼"]
@@ -22,11 +21,18 @@ class ResultViewController: UIViewController, UITableViewDelegate, UITableViewDa
     var teruteruitems: [String] = ["てるてる"]
     
     var allroleitems: [String] = ["人狼", "村人", "怪盗", "占い師", "てるてる"]
-    var activeroleArray: [String] = []
+    //var activeroleArray: [String] = []
     
     var werewolfteamArray: [String] = []
     var villagerteamArray: [String] = []
     var teruteruteamArray: [String] = []
+    var villagerteamroleArray: [String] = []
+    
+    var werewolfteamVoteArray: [String] = []
+    var villagerteamVoteArray: [String] = []
+    var teruteruteamVoteArray: [String] = []
+    
+    var votedDataArray: [String] = []
     
     var myPlayerItems: [String] = []//プレイヤーの名前
     let saveData = NSUserDefaults.standardUserDefaults()//諸々のNSUserDefaults
@@ -45,52 +51,29 @@ class ResultViewController: UIViewController, UITableViewDelegate, UITableViewDa
         myPlayerItems = saveData.arrayForKey("allPlayers")! as! [String]//プレイヤーの名前を取得して[String]に代入
         rolenumberArray = saveData.arrayForKey("RoleData")! as! [Int] //役職の人数を取得してNSArrayに代入
         roleofplayerArray = saveData.arrayForKey("RolePlayerData")! as! [String]//プレイヤーの役職を代入
-        activeroleArray = saveData.arrayForKey("ActiveRole")! as! [String]//使われている役職
+        votedDataArray = saveData.arrayForKey("votedPlayer")! as! [String]//投票情報
         print("プレイヤー:\(myPlayerItems)")
         print("役職の人数:\(rolenumberArray)")
         print("\(roleofplayerArray)")
-        print("\(activeroleArray)")
+        print("\(votedDataArray)")
         
         let rolexib = UINib(nibName: "ResultTableViewCell", bundle: nil)
         mytableview.registerNib(rolexib, forCellReuseIdentifier: "ResultCell")
         
-        //インデックスを役職順に並び替えるメソッド
-        /*
-        var copyrolenumberArray: [Int] = rolenumberArray
-        var copyroleofplayerArray: [String] = roleofplayerArray
-        var copymyPlayerItems: [String] = myPlayerItems
-        var copyallroleitems: [String] = allroleitems
-        
-        while playerdisplayArray.count != myPlayerItems.count {
-            while copyrolenumberArray[0] != 0 {
-                let foundedindex: Int! = copyroleofplayerArray.indexOf(copyallroleitems[0])
-                playerdisplayArray.append(copymyPlayerItems[foundedindex])
-                copymyPlayerItems.removeAtIndex(foundedindex)
-                copyroleofplayerArray.removeAtIndex(foundedindex)
-                copyrolenumberArray[0] = copyrolenumberArray[0] - 1
-                
-            }
-            copyrolenumberArray.removeFirst()
-            copyallroleitems.removeFirst()
-            
-            while copyrolenumberArray[0] == 0 {
-                copyrolenumberArray.removeFirst()
-                copyallroleitems.removeFirst()
-                
-            }
-        }
-        */
-        //並べ替える必要なし
-        
+        //プレイヤーをチームごとに振り分け
         var continuingnumber: Int = 0
         
         while continuingnumber != myPlayerItems.count {
             if roleofplayerArray[continuingnumber] == "人狼" {
                 werewolfteamArray.append(myPlayerItems[continuingnumber])
+                werewolfteamVoteArray.append(votedDataArray[continuingnumber])
             } else if roleofplayerArray[continuingnumber] == "てるてる" {
                 teruteruteamArray.append(myPlayerItems[continuingnumber])
+                teruteruteamVoteArray.append(votedDataArray[continuingnumber])
             } else {
                 villagerteamArray.append(myPlayerItems[continuingnumber])
+                villagerteamroleArray.append(roleofplayerArray[continuingnumber])
+                villagerteamVoteArray.append(votedDataArray[continuingnumber])
             }
             
             continuingnumber += 1 //plus 1
@@ -99,6 +82,7 @@ class ResultViewController: UIViewController, UITableViewDelegate, UITableViewDa
         print("\(werewolfteamArray)")
         print("\(villagerteamArray)")
         print("\(teruteruteamArray)")
+        print("\(villagerteamroleArray)")
         
     }
 
@@ -146,28 +130,28 @@ class ResultViewController: UIViewController, UITableViewDelegate, UITableViewDa
         let cell = tableView.dequeueReusableCellWithIdentifier("ResultCell", forIndexPath: indexPath) as! ResultTableViewCell
         
         if indexPath.section == 0 {
-            cell.roleLabel!.text = "\(allroleitems[0])"
+            cell.roleLabel!.text = "役職:\(allroleitems[0])"
         } else if indexPath.section == 1 {
-            cell.roleLabel!.text = "\(activeroleArray[indexPath.row + rolenumberArray[0]])"
+            cell.roleLabel!.text = "役職:\(villagerteamroleArray[indexPath.row])"
         } else if indexPath.section == 2 {
-            cell.roleLabel!.text = "\(allroleitems[4])"
+            cell.roleLabel!.text = "役職:\(allroleitems[4])"
         }
         //役職名を表示
         
         if indexPath.section == 0 {
-            //cell.voteLabel!.text = "\()"
+            cell.nameLabel!.text = "\(werewolfteamArray[indexPath.row])"
         } else if indexPath.section == 1 {
-            
+            cell.nameLabel!.text = "\(villagerteamArray[indexPath.row])"
         } else if indexPath.section == 2 {
-            
+            cell.nameLabel!.text = "\(teruteruteamArray[indexPath.row])"
         }
         
         if indexPath.section == 0 {
-            
+            cell.voteLabel!.text = "\(werewolfteamVoteArray[indexPath.row])に投票しました"
         } else if indexPath.section == 1 {
-            
+            cell.voteLabel!.text = "\(villagerteamVoteArray[indexPath.row])に投票しました"
         } else if indexPath.section == 2 {
-            
+            cell.voteLabel!.text = "\(teruteruteamVoteArray[indexPath.row])に投票しました"
         }
         
         if indexPath.section == 0 {
